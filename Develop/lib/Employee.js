f35daf6b2495f193364d3f6d7c9276546d238ffb
Andrew Bjork <bjorkandrew1@gmail.com>
2020-02-29 21:36:58 -0600

    updating

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+  constructor(employee) {
+    this.employeetype = employee;
+
+    
+  }
+}
+
+engineerPerson = new Employee("Engineer");
+internPerson = new Employee("Intern");
+managerPerson = new Employee("Manager");
+
+console.log(engineerPerson);
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,3 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+
